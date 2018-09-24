3ce231dac3882309e15ad08e70f715487174c241
LAPTOP-UFAJF8VA\Patrycja Karbownik <patrycja@karbownik.net>
2018-09-24 23:40:56 +0200

    procedury - zakonczenie czesci teoretycznej, zaliczenie egz wewnetrznego, jesli teoria zakonczona

--- sql/insert_trainee.sql
+++ sql/insert_trainee.sql
@@ -4,11 +4,10 @@ INSERT INTO PERSON (Pesel, Surname, Name, Birthday, ID_addr)
 INSERT INTO ADDRESS (City, Street, Building_number)
 	VALUES ('Henrysin', 'Henrysin', 9);
 	
-INSERT INTO TRAINEE (Pesel, Starting_date, Theory, Internal_exam) --TRG age
+INSERT INTO TRAINEE (Pesel, Starting_date, Theory, Internal_exam)
 	VALUES ('96090912013', to_date('2014-09-25', 'yyyy-mm-dd'), 'T', 'T');
 	
 	
---data kolejna od rozpoczecia kursu (pierwsze jazdy, potem losowo co 1,2, (3, 4) dni
 INSERT INTO LESSON (L_date, Starting_time, Area_name, ID_emp, ID_trn, ID_veh)
 	SELECT '2014-10-16', 7, Nowy Dwor Mazowiecki, 1, 1, 1 FROM dual
 	UNION ALL SELECT '2014-10-17', 13, Nowy Dwor Mazowiecki, 1, 1, 1 FROM dual
@@ -29,3 +28,34 @@ INSERT INTO EXAM (E_date, ID_word, ID_trn, E_type, Pass_or_not)
 	UNION ALL SELECT '2014-12-01', 1, 1, 'practice', 'not' FROM dual
 	UNION ALL SELECT '2014-12-21', 1, 1, 'practice', 'pass' FROM dual
 	;
+	
+
+INSERT INTO ADDRESS (City, Street, Building_number, Flat_number)
+	VALUES ('Ustka', 'Falata', 10, 50);
+	
+INSERT INTO PERSON (Pesel, Surname, Name, Birthday, ID_addr)
+	VALUES ('73081425301', 'Pozeczka', 'Izabella', to_date('1973-08-14', 'yyyy-mm-dd'), 66);
+	
+INSERT INTO TRAINEE (Pesel, Starting_date)
+	VALUES ('73081425301', to_date('2018-09-14', 'yyyy-mm-dd'));
+	
+	
+INSERT INTO ADDRESS (City, Street, Building_number)
+	VALUES ('Slupsk', 'Szczecinska', 100);
+	
+INSERT INTO PERSON (Pesel, Surname, Name, Birthday, ID_addr)
+	VALUES ('97011021423', 'Wisnia', 'Ilona', to_date('1997-01-10', 'yyyy-mm-dd'), 67);
+	
+INSERT INTO TRAINEE (Pesel, Starting_date)
+	VALUES ('97011021423', to_date('2018-09-14', 'yyyy-mm-dd'));
+	
+	
+	
+INSERT INTO ADDRESS (City, Street, Building_number, Flat_number)
+	VALUES ('Ustka', 'Slupska', 9, 6);
+	
+INSERT INTO PERSON (Pesel, Surname, Name, Birthday, ID_addr)
+	VALUES ('92090801317', 'Perka', 'Adam', to_date('1992-09-08', 'yyyy-mm-dd'), 68);
+	
+INSERT INTO TRAINEE (Pesel, Starting_date)
+	VALUES ('92090801317', to_date('2018-09-14', 'yyyy-mm-dd'));
---
+++ sql/procedures.sql
@@ -0,0 +1,43 @@
+CREATE OR REPLACE PROCEDURE finish_theory(pesel_out VARCHAR2)
+IS
+BEGIN
+	UPDATE TRAINEE
+	SET Theory = 'T'
+	WHERE Pesel = pesel_out AND Theory = 'F';
+END;
+
+execute finish_theory(xxx);
+
+
+
+CREATE OR REPLACE PROCEDURE pass_internal_exam(pesel_out VARCHAR2)
+IS
+BEGIN
+    DECLARE
+    notTheory EXCEPTION;
+    th CHAR;
+    PRAGMA EXCEPTION_INIT(notTheory, -20101);
+    BEGIN
+        BEGIN
+            SELECT Theory INTO th
+            FROM TRAINEE
+            WHERE Pesel = pesel_out;
+            
+            IF th = 'F' THEN
+                raise_application_error(-20101, 'Theory is not finished');
+            END IF;
+        
+            UPDATE TRAINEE
+            SET Internal_Exam = 'T'
+            WHERE Pesel = pesel_out AND Internal_Exam = 'F';
+        END;
+        EXCEPTION
+            WHEN notTheory THEN
+                dbms_output.put_line(SQLERRM);
+                raise;
+    END;
+END;
+
+execute pass_internal_exam(xxx);
+
+
