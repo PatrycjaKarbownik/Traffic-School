3b603bcd77b58af59beceed470cefd7d05a14e24
LAPTOP-UFAJF8VA\Patrycja Karbownik <patrycja@karbownik.net>
2018-09-25 23:04:54 +0200

    poczatek aplikacji

---
+++ App/src/Main.java
@@ -0,0 +1,22 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    private static Stage primaryStage;
+
+    public static Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+        System.exit(0);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        this.primaryStage = primaryStage;
+        AppWindow window = new AppWindow(primaryStage);
+    }
+}
